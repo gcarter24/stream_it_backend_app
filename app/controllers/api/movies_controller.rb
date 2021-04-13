9c264fb7ceeb7b8ea67cd341736c4c1e7ca3777c
George <gchriscarter@gmail.com>
2021-04-13 15:20:44 -0400

    allow cors access, allow show and index to show only network name, and cycle through all movies to select one random movie

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,6 +1,21 @@
 class Api::MoviesController < ApplicationController
   def index
-    @movies = Movie.order(:id => :asc)
+    @movies_year = Movie.where({ year: params[:search] })
+    @movies_rating = Movie.where({ rating: params[:search] })
+    @movies_type = Movie.where({ media_type: params[:search] })
+    @movies_language = Movie.where({ language: params[:search] })
+
+    if @movies == @movies_year
+      return @movies_year.sample
+    elsif @movies == @movies_rating
+      return @movies_rating.sample
+    elsif @movies == @movies_type
+      return @movies_type.sample
+    elsif @movies == @movies_language
+      return @movies_language.sample
+    else
+      @movies = Movie.order(:id => :asc)
+    end
 
     render "index.json.jb"
   end
@@ -13,19 +28,22 @@ class Api::MoviesController < ApplicationController
 
   def random
     # random movie
-    # @movie = Movie.find(Movie.pluck(:id).sample)
+    @movie = Movie.find(Movie.pluck(:id).sample)
 
     # random movie based on year
-    @movie = Movie.where({ year: params[:search] }).sample
+    # @movie = Movie.where({ year: params[:search] }).sample
 
-    # random movie based on rating
-    @movie = Movie.where(rating: params[:search]).sample
+    # # random movie based on rating
+    # @movie = Movie.where(rating: params[:search]).sample
 
-    # random movie based on media type
-    @movie = Movie.where(media_type: params[:search]).sample
+    # # random movie based on media type
+    # @movie = Movie.where(media_type: params[:search]).sample
 
-    # random movie based on language
-    @movie = Movie.where(language: params[:search]).sample
+    # # random movie based on language
+    # @movie = Movie.where(language: params[:search]).sample
+
+    # random movie based on runtime
+    # @movie = Movie.where(runtime_minutes: params[:search]).sample
 
     #random movie based on network(id,  then later name)
     #random movie based on genre(id,  then later name)
