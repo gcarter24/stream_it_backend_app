7b52ccd96fa6e77a832738aced6c164ba84cb004
George <gchriscarter@gmail.com>
2021-04-16 18:01:07 -0400

    add test logic to controller to search by individual params, and get working, added genres to movie_id: 60

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -12,27 +12,6 @@ class Api::MoviesController < ApplicationController
   end
 
   def random
-    # random movie
-    # @movie = Movie.find(Movie.pluck(:id).sample)
-    # random movie based on year
-    @movies_year = Movie.where(year: params[:year])
-    # # random movie based on rating
-    @movies_rating = Movie.where(rating: params[:rating])
-    # # random movie based on media type
-    @movies_type = Movie.where(media_type: params[:media_type])
-    # # random movie based on language
-    @movies_language = Movie.where(language: params[:language])
-    # # random movie based on runtime
-    @movies_runtime = Movie.where(runtime_minutes: params[:runtime_minutes])
-    # # random movie based on network
-    @movie_network = Network.find_by(name: params[:network])
-    # # @movie_by_network = @movie_network.movies
-
-    # # random movie based on genre
-    @movies = Movie.all
-    @movie_genre = Genre.find_by(name: params[:genre])
-    # @movie_genre = @movie_genre.movies
-
     # CORRECT SQL QUERY FOR MOVIE, NETWORK, AND GENRE
     # SELECT "movies".* FROM "movies" WHERE "movies"."year" = $1 AND "movies"."rating" = $2 AND "movies"."language" = $3 AND "movies"."runtime_minutes" = $4 AND "movies"."media_type" = $5
     #SELECT "movies".* FROM "movies" INNER JOIN "movie_genres" ON "movies"."id" = "movie_genres"."movie_id" WHERE "movie_genres"."genre_id" = $1
@@ -56,44 +35,52 @@ class Api::MoviesController < ApplicationController
     @movie = @m & genre_movies & network_movies
     @final_movie = @movie.sample
 
-    # @params = {
-    #   year: params[:year],
-    #   rating: params[:rating],
-    #   language: params[:language],
-    #   runtime_minutes: params[:runtime_minutes],
-    #   media_type: params[:media_type],
-    #   genre: params[:genre],
-    #   network: params[:network],
-    # }
-
-    # @movie.sample
-
-    # @movie = Movie.where({
-    #   # year: params[:year],
-    #   rating: params[:rating],
-    #   language: params[:language],
-    #   # runtime_minutes: params[:runtime_minutes],
-    #   media_type: params[:media_type],
-    # }).sample
-
-    # if params[:search] == @movies_year
-    #   @movie = @movies_year.sample
-    # elsif params[:search] == @movies_rating
-    #   @movie = @movies_rating.sample
-    # elsif params[:search] == @movies_type
-    #   @movie = @movies_type.sample
-    # elsif params[:search] == @movies_language‹
-    #   @movie = @movies_language.sample
-    # elsif params[:search] == @movies_runtime
-    #   @movie = @movies_runtime.sample
-    #   # elsif @movie == @movie_network
-    #   #   @movie_by_network.sample
-    #   # elsif @movie == @movie_genre
-    #   #   @movie_genre.sample
-    # else
-    @movie = Movie.find(Movie.pluck(:id).sample)
-    # end
-
     render "random.json.jb"
+  end
+
+  def test
+    # random movie
+    movies = Movie.all
+    @movie = Movie.find(Movie.pluck(:id).sample)
+    # random movie based on year
+    @movies_year = Movie.where(year: params[:year]).sample
+    # # # random movie based on rating
+    @movies_rating = Movie.where(rating: params[:rating]).sample
+    # # # random movie based on media type
+    @movies_type = Movie.where(media_type: params[:media_type]).sample
+    # # # random movie based on language
+    @movies_language = Movie.where(language: params[:language]).sample
+    # # # random movie based on runtime
+    @movies_runtime = Movie.where(runtime_minutes: params[:runtime_minutes]).sample
+    # # # random movie based on network
+    # @network = Network.find_by(name: params[:network])
+    # @network_movies = @network.movies
+    # # # @movie_by_network = @movie_network.movies
+
+    # # # random movie based on genre
+    # @genre = Genre.find_by(name: params[:genre])
+    # @genre_movies = @genre.movies
+
+    if params[:year]
+      @final_movie = @movies_year
+    elsif params[:rating]
+      @final_movie = @movies_rating
+    elsif params[:media_type]
+      @final_movie = @movies_type
+    elsif params[:runtime_minutes]
+      @final_movie = @movies_runtime
+    elsif params[:language]
+      @final_movie = @movies_language
+    elsif params[:genre]
+      @genre = Genre.find_by(name: params[:genre])
+      @genre_movies = @genre.movies
+      @final_movie = @genre_movies.sample
+    elsif params[:network]
+      @network = Network.find_by(name: params[:network])
+      @network_movies = @network.movies
+      @final_movie = @network_movies.sample
+    else
+      @final_movie = Movie.find(Movie.pluck(:id).sample)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     get "/movies" => "movies#index"
     get "/movies/random" => "movies#random"
+    get "/movies/test" => "movies#test"
     get "/movies/:id" => "movies#show"
     get "/networks" => "networks#index"
     get "/networks/:id" => "networks#show"
